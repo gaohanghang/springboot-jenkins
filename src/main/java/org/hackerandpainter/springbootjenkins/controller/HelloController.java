549e47b39167064190edccc674bb6aab26aafd56
gaohanghang <1341947277@qq.com>
2019-07-31 22:35:55 +0800

    first commit

---
+++ src/main/java/org/hackerandpainter/springbootjenkins/controller/HelloController.java
@@ -0,0 +1,18 @@
+package org.hackerandpainter.springbootjenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author Gao Hang Hang
+ * @Date 2019-07-31 22:33
+ **/
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+}
